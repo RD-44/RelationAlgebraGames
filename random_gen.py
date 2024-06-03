40d98a8598bc14a745413e845c554b3d1b9893c7
Aadhavan Mamallan <108578027+RD-44@users.noreply.github.com>
2024-06-04 00:00:41 +0100

    Shuffled atoms before converse
    - To get truly random converses, atoms are shuffled randomly using a Numpy function, before consecutive pairs are taken to be converse pairs.

--- random_gen.py
+++ random_gen.py
@@ -1,6 +1,7 @@
 import random
+import numpy as np
 
-class Elem:
+class Elem: # takes in RA
     def __init__(self, i, base_size):
         if base_size == 0 or (base_size & (base_size - 1) != 0):
             raise Exception("Error - base_size must be power of 2.")
@@ -18,12 +19,17 @@ class Elem:
     
     def __neg__(self):
         return Elem(self.base_size - 1 - self.i, self.base_size)
+
+    def __invert__(self):
+        pass
+    # converse
     
     def __le__(self, other):
         return self + other == other
 
     def isaatom(self):
         return (self.i != 0 and (self.i & (self.i - 1) == 0))
+    
 
     def __eq__(self, other):
         return self.i == other.i
@@ -36,25 +42,19 @@ class RA:
         self.converses = converses
         self.forbidden = forbidden
 
-    
-    
-
-
 def randomRA():
     num_atoms = random.randint(2, 10)
     num_converse_pairs = random.randint(0, num_atoms//2)
     print("Atoms: ", num_atoms)
     print("Non-self converse atoms: ", num_converse_pairs*2)
     end = num_converse_pairs*2
-    converses = {i : i+1 for i in range(0, end, 2)} | {i+1:i for i in range(0, end, 2)} | {i : i for i in range(end, num_atoms)}
-
-
-
+    atoms = np.array([i for i in range(num_atoms)])
+    np.random.shuffle(atoms)
+    converses = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
 
     assert(len(converses)==num_atoms)
     print(converses)
 
 randomRA()
     
-print(~3)
 # think about how to represent forbidden triples. Also ask in the next meeting how to check for associativity
