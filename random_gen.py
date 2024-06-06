85b90e3735533c406b54cdb473f7f02c7c02e308
Aadhavan Mamallan <108578027+RD-44@users.noreply.github.com>
2024-06-06 23:21:50 +0100

    Changed the way compositions are generated
    - Changed how composition is generated to deal with the identity
    - Randomly chooses the number of self converse atoms below the identity, then checks all the legal triples which are possible and partitions into groups of piercean transforms

--- random_gen.py
+++ random_gen.py
@@ -1,5 +1,13 @@
 import random
 import numpy as np
+import itertools as it
+import matplotlib.pyplot as plt
+import pandas as pd
+
+"""
+ Are all the units self converse?
+Must there be at least one self converse atom
+"""
 
 class Elem: # element of relation algebra
     def __init__(self, i, size):
@@ -41,31 +49,84 @@ class RA:
         self.converses = converses
         self.composition = composition
 
+def peircean(triple, conv): # returns a set of all peircean transforms of a given triple
+    a, b, c = triple
+    return set([
+        (a, b, c),
+        (b, conv[c], conv[a]),
+        (conv[c], a, conv[b]),
+        (conv[b], conv[a], conv[c]),
+        (conv[a], c, b),
+        (c, conv[b], a)
+        ])
+    
 def randomRA():
-    num_atoms = 3 #random.randint(2, 10)
-    num_converse_pairs = random.randint(0, num_atoms//2)
+    num_atoms = random.randint(2, 5)
+    num_converse_pairs = random.randint(0, num_atoms//2 if num_atoms % 2 != 0 else num_atoms//2 - 1)
+    num_self_converse = num_atoms - 2*num_converse_pairs
     print("Atoms: ", num_atoms)
-    print("Non-self converse atoms: ", num_converse_pairs*2)
+    print("Number of self converse atoms: ", num_self_converse)
     end = num_converse_pairs*2
-    atoms = np.array([chr(97+i) for i in range(num_atoms)])
+    atoms = np.array([i for i in range(num_atoms)]) #chr(97+i) 
+
     np.random.shuffle(atoms)
+    conv = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
 
-    converse = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
-    composition = [[set() for _ in range(num_atoms)] for _ in range(num_atoms)] # composition table
+    self_converse_atoms = atoms[end:]
+    np.random.shuffle(self_converse_atoms)
+    num_atoms_below_id = random.randint(1, num_self_converse)
+    
+    atoms_below_id = self_converse_atoms[:num_atoms_below_id]
+    print(atoms_below_id)
 
-    for i in range(num_atoms):
-        for j in range(num_atoms):
-            for k in range(num_atoms):
-                if random.choice([True, False]):
-                    composition[i][j].add(converse[atoms[k]])
-                    composition[j][k].add(converse[atoms[i]])
-                    composition[k][i].add(converse[atoms[j]])
-                    
+    perms = set(it.product(atoms, repeat=3))
+
+    groups = set()
+
+    illegal = set()
+
+    legal = set()
+
+    for e in atoms_below_id:
+        for i in range(num_atoms):
+            legal.add(frozenset(peircean((e, atoms[i], atoms[i]), conv)))
+            for j in range(i+1, num_atoms):
+                illegal.add((e, atoms[i], atoms[j]))
+
+    while len(perms) > 0:
+        curr = perms.pop()
+        group = frozenset(peircean(curr, conv))
+        perms = perms.difference(group)
+        if len(group.intersection(illegal)) == 0 and group not in legal:
+            groups.add(group)
+
+    for g in groups:
+        if random.choice([True, False]):
+            legal.add(g)
+
+
+    table = [["" for _ in range(num_atoms)] for _ in range(num_atoms)]
+
+    tochar = {}
+    for i in range(num_atoms_below_id):
+        tochar[atoms_below_id[i]] = 'e' + str(i)
+    i = 0
+    for j in range(num_atoms_below_id, num_self_converse):
+        tochar[self_converse_atoms[j]] = 'x' + str(i)
+        i += 1
+    for j in range(end):
+        tochar[atoms[j]] = 'x' + str(i)
+        i += 1
+    for l in legal:
+        for triple in l:
+            table[triple[0]][triple[1]] += '+' + tochar[triple[2]] if table[triple[0]][triple[1]] else tochar[triple[2]] 
+
+    df = pd.DataFrame(table)
+    df = df.rename(columns=tochar, index=tochar)
+    df = df.reindex(sorted(df.columns), axis=1)
+    df.sort_index(inplace=True)
         
-    for row in composition:
-        print(*row, sep="\t")
-
-    print(converse)
-
+    print(df)
+    
 randomRA()
 
