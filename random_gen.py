9e5b759f466eb0a101d1a4e6d22a88a56af793aa
Aadhavan Mamallan <108578027+RD-44@users.noreply.github.com>
2024-06-07 12:05:55 +0100

    Refactoring and made certain triple consistent
    - Made the triple (ei, a, a) to be consistent for any atom ei.
    - Tidied up code, moved formatting stuff into its own function.

--- random_gen.py
+++ random_gen.py
@@ -7,7 +7,106 @@ import pandas as pd
 """
  Are all the units self converse?
 Must there be at least one self converse atom
+Must (ei, a, a) be consistent, can i choose to forbid it
+
+
 """
+    
+def format(units, diversity_atoms, legal):
+    num_atoms = len(units) + len(diversity_atoms)
+    table = [['0' for _ in range(num_atoms)] for _ in range(num_atoms)]
+
+    SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+    tochar = {}
+    i = 0
+    for e in units:
+        tochar[e] = 'e' + str(i).translate(SUB)
+        i += 1
+
+    if len(units) == 1:
+        e = units.pop()
+        tochar[e] = "1'"
+        units.add(e)
+
+    i = 0
+    for d in diversity_atoms:
+        tochar[d] = 'x' + str(i).translate(SUB)
+        i += 1
+
+    for l in legal:
+        for triple in l:
+            table[triple[0]][triple[1]] = table[triple[0]][triple[1]] + '+' + tochar[triple[2]] if table[triple[0]][triple[1]]!='0' else tochar[triple[2]] 
+            if table[triple[0]][triple[1]].count('+') == num_atoms - 1:
+                table[triple[0]][triple[1]] = '1'
+
+    df = pd.DataFrame(table)
+    df = df.rename(columns=tochar, index=tochar)
+    df = df.reindex(sorted(df.columns), axis=1)
+    df.sort_index(inplace=True)
+
+    return df
+
+def peircean(triple, conv): # returns a set of all peircean transforms of a given triple
+    a, b, c = triple
+    return set([
+        (a, b, c),
+        (b, conv[c], conv[a]),
+        (conv[c], a, conv[b]),
+        (conv[b], conv[a], conv[c]),
+        (conv[a], c, b),
+        (c, conv[b], a)
+        ])
+    
+def randomRA():
+    num_atoms = 3 #random.randint(2, 5)
+    num_converse_pairs = 0 # random.randint(0, num_atoms//2 if num_atoms % 2 != 0 else num_atoms//2 - 1)
+    num_self_converse = num_atoms - 2*num_converse_pairs
+    print("Atoms: ", num_atoms)
+    print("Number of self converse atoms: ", num_self_converse)
+    end = num_converse_pairs*2
+    atoms = np.array([i for i in range(num_atoms)]) 
+
+    np.random.shuffle(atoms)
+
+    # random composition table
+    conv = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
+
+    self_converse_atoms = atoms[end:]
+    np.random.shuffle(self_converse_atoms)
+    num_atoms_below_id = random.randint(1, num_self_converse)
+    
+    atoms_below_id = self_converse_atoms[:num_atoms_below_id]
+
+    options = set()
+    illegal = set()
+    legal = set()
+    units = set(atoms_below_id)
+    diversity_atoms = set(atoms).difference(units)
+
+    for e in atoms_below_id:
+        for i in range(num_atoms):
+            if (atoms[i] not in units) or (atoms[i] in units and atoms[i] == e) : legal.add(frozenset(peircean((e, atoms[i], atoms[i]), conv)))
+            for j in range(i+1, num_atoms):
+                illegal.add((e, atoms[i], atoms[j])) # ei;a * b = 0 if a != b
+    
+    permutations = set(it.product(atoms, repeat=3)) # cartesian product (set of atoms)^3
+
+    while len(permutations) > 0: # group permutations into peircean groups
+        curr = permutations.pop()
+        group = frozenset(peircean(curr, conv))
+        permutations = permutations.difference(group)
+        if len(group.intersection(illegal)) == 0 and group not in legal:
+            options.add(group)
+
+    for g in options:
+        if random.choice([True, False]):
+            legal.add(g)
+
+    df = format(units, diversity_atoms, legal)
+
+    print(df)
+    
+randomRA()
 
 class Elem: # element of relation algebra
     def __init__(self, i, size):
@@ -40,93 +139,5 @@ class Elem: # element of relation algebra
 
     def __eq__(self, other):
         return self.i == other.i
-    
 
-class RA:
-
-    def __init__(self, base_size, composition, converses) -> None:
-        self.base_size = base_size
-        self.converses = converses
-        self.composition = composition
-
-def peircean(triple, conv): # returns a set of all peircean transforms of a given triple
-    a, b, c = triple
-    return set([
-        (a, b, c),
-        (b, conv[c], conv[a]),
-        (conv[c], a, conv[b]),
-        (conv[b], conv[a], conv[c]),
-        (conv[a], c, b),
-        (c, conv[b], a)
-        ])
-    
-def randomRA():
-    num_atoms = random.randint(2, 5)
-    num_converse_pairs = random.randint(0, num_atoms//2 if num_atoms % 2 != 0 else num_atoms//2 - 1)
-    num_self_converse = num_atoms - 2*num_converse_pairs
-    print("Atoms: ", num_atoms)
-    print("Number of self converse atoms: ", num_self_converse)
-    end = num_converse_pairs*2
-    atoms = np.array([i for i in range(num_atoms)]) #chr(97+i) 
-
-    np.random.shuffle(atoms)
-    conv = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
-
-    self_converse_atoms = atoms[end:]
-    np.random.shuffle(self_converse_atoms)
-    num_atoms_below_id = random.randint(1, num_self_converse)
-    
-    atoms_below_id = self_converse_atoms[:num_atoms_below_id]
-    print(atoms_below_id)
-
-    perms = set(it.product(atoms, repeat=3))
-
-    groups = set()
-
-    illegal = set()
-
-    legal = set()
-
-    for e in atoms_below_id:
-        for i in range(num_atoms):
-            legal.add(frozenset(peircean((e, atoms[i], atoms[i]), conv)))
-            for j in range(i+1, num_atoms):
-                illegal.add((e, atoms[i], atoms[j]))
-
-    while len(perms) > 0:
-        curr = perms.pop()
-        group = frozenset(peircean(curr, conv))
-        perms = perms.difference(group)
-        if len(group.intersection(illegal)) == 0 and group not in legal:
-            groups.add(group)
-
-    for g in groups:
-        if random.choice([True, False]):
-            legal.add(g)
-
-
-    table = [["" for _ in range(num_atoms)] for _ in range(num_atoms)]
-
-    tochar = {}
-    for i in range(num_atoms_below_id):
-        tochar[atoms_below_id[i]] = 'e' + str(i)
-    i = 0
-    for j in range(num_atoms_below_id, num_self_converse):
-        tochar[self_converse_atoms[j]] = 'x' + str(i)
-        i += 1
-    for j in range(end):
-        tochar[atoms[j]] = 'x' + str(i)
-        i += 1
-    for l in legal:
-        for triple in l:
-            table[triple[0]][triple[1]] += '+' + tochar[triple[2]] if table[triple[0]][triple[1]] else tochar[triple[2]] 
-
-    df = pd.DataFrame(table)
-    df = df.rename(columns=tochar, index=tochar)
-    df = df.reindex(sorted(df.columns), axis=1)
-    df.sort_index(inplace=True)
-        
-    print(df)
-    
-randomRA()
 
