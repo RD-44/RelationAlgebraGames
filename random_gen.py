27e64682fba7fe7612f0c89cfe95665c07cd24ce
Aadhavan Mamallan <108578027+RD-44@users.noreply.github.com>
2024-06-10 13:00:15 +0100

    Completed random generator for Non-Associative RAs
    - Used the following algorithm: Randomly choose number of units and diversity atoms. Randomly pick the start and end for each atom and converse pairs which respect the start and ends. Compute the triples which must be legal and illegal and the ones which we are free to forbid or include, partitioning these into Peircean groups. Then randomly select pericean groups to include.
    - Next step is to check if RA associative, and if not then throw it away and generate again

--- random_gen.py
+++ random_gen.py
@@ -3,141 +3,131 @@ import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
 import pandas as pd
-
-"""
- Are all the units self converse?
-Must there be at least one self converse atom
-Must (ei, a, a) be consistent, can i choose to forbid it
-
-
-"""
     
-def format(units, diversity_atoms, legal):
-    num_atoms = len(units) + len(diversity_atoms)
+def format(num_units, diversity, legal, converse): # creates nicely formatted composition and converse table
+    num_atoms = num_units + len(diversity)
     table = [['0' for _ in range(num_atoms)] for _ in range(num_atoms)]
 
     SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
     tochar = {}
     i = 0
-    for e in units:
+    for e in range (num_units):
         tochar[e] = 'e' + str(i).translate(SUB)
         i += 1
 
-    if len(units) == 1:
-        e = units.pop()
-        tochar[e] = "1'"
-        units.add(e)
+    if num_units == 1:
+        tochar[0] = "1'"
 
     i = 0
-    for d in diversity_atoms:
+    for d in diversity:
         tochar[d] = 'x' + str(i).translate(SUB)
         i += 1
 
+    triples = []
     for l in legal:
         for triple in l:
-            table[triple[0]][triple[1]] = table[triple[0]][triple[1]] + '+' + tochar[triple[2]] if table[triple[0]][triple[1]]!='0' else tochar[triple[2]] 
-            if table[triple[0]][triple[1]].count('+') == num_atoms - 1:
-                table[triple[0]][triple[1]] = '1'
+            triples.append(triple)
 
-    df = pd.DataFrame(table)
-    df = df.rename(columns=tochar, index=tochar)
-    df = df.reindex(sorted(df.columns), axis=1)
-    df.sort_index(inplace=True)
+    triples.sort(key = lambda triple : (triple[0], triple[1], converse[triple[2]]))
 
-    return df
+    for triple in triples:
+        table[triple[0]][triple[1]] = table[triple[0]][triple[1]] + '+' + tochar[converse[triple[2]]] if table[triple[0]][triple[1]]!='0' else tochar[converse[triple[2]]] 
+        if table[triple[0]][triple[1]].count('+') == num_atoms - 1:
+            table[triple[0]][triple[1]] = '1'
 
-def peircean(triple, conv): # returns a set of all peircean transforms of a given triple
+    composition_table = pd.DataFrame(table)
+    composition_table.rename(columns=tochar, index=tochar, inplace=True)
+    composition_table = composition_table.reindex(sorted(composition_table.columns), axis=1)
+    composition_table.sort_index(inplace=True)
+    composition_table.columns.name = ';'
+
+    converse_table = pd.DataFrame([tochar[converse[x]] for x in converse], index=[tochar[x] for x in range(num_atoms)], columns=['a︶'])
+    converse_table.columns.name= 'a'
+
+    return composition_table, converse_table
+
+def peircean(triple, converse): # returns a set of all peircean transforms of a given triple
     a, b, c = triple
     return set([
         (a, b, c),
-        (b, conv[c], conv[a]),
-        (conv[c], a, conv[b]),
-        (conv[b], conv[a], conv[c]),
-        (conv[a], c, b),
-        (c, conv[b], a)
+        (b, c, a),
+        (c, a, b),
+        (converse[b], converse[a], converse[c]),
+        (converse[a], converse[c], converse[b]),
+        (converse[c], converse[b], converse[a])
         ])
     
-def randomRA():
-    num_atoms = 3 #random.randint(2, 5)
-    num_converse_pairs = 0 # random.randint(0, num_atoms//2 if num_atoms % 2 != 0 else num_atoms//2 - 1)
-    num_self_converse = num_atoms - 2*num_converse_pairs
-    print("Atoms: ", num_atoms)
-    print("Number of self converse atoms: ", num_self_converse)
-    end = num_converse_pairs*2
-    atoms = np.array([i for i in range(num_atoms)]) 
+def generateRA():
+    num_units = 1 #random.randint(1, 2) # number of atoms below the identity 
+    num_divs = 2 # number of diversity atoms
+    num_atoms = num_units + num_divs 
 
-    np.random.shuffle(atoms)
+    atoms = [i for i in range(num_atoms)] # number the atoms
+    random_unit = lambda : random.randint(0, num_units-1)
+    isunit = lambda x : 0 <= x < num_units # predicate to check if number corresponds to unit
+    diversity = [i+num_units for i in range(num_divs)]
 
-    # random composition table
-    conv = {atoms[i] : atoms[i+1] for i in range(0, end, 2)} | {atoms[i+1] : atoms[i] for i in range(0, end, 2)} | {atoms[i]  : atoms[i]  for i in range(end, num_atoms)}
+    # these dictionaries store start and end of each diversity atom. If d = e_0 ; d : e_1 then starts[d] = e_0
+    start = {i : i for i in range(num_units)} 
+    end = {i : i for i in range(num_units)} 
+    need_converse = False
+    nextStart = None
+    nextEnd = None
+    prev = None
+    converse = {i : i for i in range(num_units)} # map of converses
 
-    self_converse_atoms = atoms[end:]
-    np.random.shuffle(self_converse_atoms)
-    num_atoms_below_id = random.randint(1, num_self_converse)
+    for i in range(num_divs): # Generate start, end and converse for each diversity atom
+        if need_converse:
+            start[diversity[i]] = nextStart
+            end[diversity[i]] = nextEnd
+            converse[prev] = diversity[i]
+            converse[diversity[i]] = prev
+            need_converse = False
+        elif i == num_divs - 1: # if reached last diversity atom and don't need to be in a converse pair, make it self-converse
+            start[diversity[i]] = random_unit()
+            end[diversity[i]] = start[diversity[i]]
+            converse[diversity[i]] = diversity[i]
+        else:
+            start[diversity[i]] = random_unit()
+            end[diversity[i]] = random_unit()
+            # If start != end, atom must not be self-converse. If not, can do whatever we want.
+            if (start[diversity[i]] != end[diversity[i]]) or random.choice([True, False]): 
+                nextStart = end[diversity[i]]
+                nextEnd = start[diversity[i]]
+                need_converse = True
+                prev = diversity[i]
+            else: 
+                converse[diversity[i]] = diversity[i]
+
+    illegal = set() # stores at least one triple from each forbidden peircean set
+    legal = set() # stores peircean sets of triples that must be consistent
+    options = set() # stores peircean sets of triples that we are free to forbid or allow
     
-    atoms_below_id = self_converse_atoms[:num_atoms_below_id]
-
-    options = set()
-    illegal = set()
-    legal = set()
-    units = set(atoms_below_id)
-    diversity_atoms = set(atoms).difference(units)
-
-    for e in atoms_below_id:
-        for i in range(num_atoms):
-            if (atoms[i] not in units) or (atoms[i] in units and atoms[i] == e) : legal.add(frozenset(peircean((e, atoms[i], atoms[i]), conv)))
-            for j in range(i+1, num_atoms):
-                illegal.add((e, atoms[i], atoms[j])) # ei;a * b = 0 if a != b
+    for a, b, c in it.product(atoms, repeat=3): # iterate through all triples
+        if start[a] != end[c] or end[b] != start[c] or end[a] != start[b]:
+            illegal.add((a, b, c))
+        elif isunit(a): 
+            if start[b] != a or b != converse[c]:
+                illegal.add((a, b, c))
+            else:
+                legal.add(frozenset(peircean((a, b, c), converse)))
+        elif not isunit(b) and not isunit(c): # adds options for not illegal triples that don'tinvolving units
+            options.add(frozenset(peircean((a, b, c), converse)))
     
-    permutations = set(it.product(atoms, repeat=3)) # cartesian product (set of atoms)^3
-
-    while len(permutations) > 0: # group permutations into peircean groups
-        curr = permutations.pop()
-        group = frozenset(peircean(curr, conv))
-        permutations = permutations.difference(group)
-        if len(group.intersection(illegal)) == 0 and group not in legal:
-            options.add(group)
-
-    for g in options:
-        if random.choice([True, False]):
-            legal.add(g)
-
-    df = format(units, diversity_atoms, legal)
-
-    print(df)
+    for l in options: 
+        if random.choice([True, False]): # randomly choose peircan sets of triples to make consistent
+            legal.add(l)
     
-randomRA()
+    composition_table, converse_table = format(num_units, diversity, legal, converse) # produce composition table and converse table
+    print("Composition: ")
+    print(composition_table)
+    print("\nConverse: ")
+    print(converse_table)
 
-class Elem: # element of relation algebra
-    def __init__(self, i, size):
-        if size == 0 or (size & (size - 1) != 0):
-            raise Exception("Error - size must be power of 2.")
-        self.i = i
-        self.size = size
-    
-    def __str__(self) -> str:
-        return str(self.i)
-    
-    def __mul__(self, other): # boolean AND
-        return Elem(self.i & other.i)
-    
-    def __add__(self, other): # boolean OR
-        return Elem(self.i | other.i)
-    
-    def __neg__(self): # boolean negation
-        return Elem(self.size - 1 - self.i, self.size)
+    # print("Units: ", unit)
+    # print("Diversity atoms: ", diversity)
+    # print("Start: ", start)
+    # print("End: ", end)
 
-    def __invert__(self): # converse (yet to implement)
-        pass
-    
-    def __le__(self, other):  # a <= b iff a + b = b
-        return self + other == other
-
-    def isaatom(self): # atoms are represented by powers of 2
-        return (self.i != 0 and (self.i & (self.i - 1) == 0))
-    
-
-    def __eq__(self, other):
-        return self.i == other.i
-
+generateRA()
 
